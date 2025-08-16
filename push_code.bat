ef505045bf599f45192250c9c7c5af7bc2578b3b
skygenai-appliedintelligence <skygenaiappliedintelligence@gmail.com>
2025-08-16 21:16:40 +0800

    Delete push_code.bat

--- push_code.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Pushing code to Hire-GenAI repository...
-git add .
-git commit -m "Update feature locking starter code"
-git push origin dev:main --force
-echo Code pushed successfully!
-pause
