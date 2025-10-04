18181bbc80257492e187f76f9fb0ee22804bc78d
skygenai-appliedintelligence <skygenaiappliedintelligence@gmail.com>
2025-10-04 18:38:08 +0800

    CV parseing done for word and pdf

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,26 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude pdf-parse test files from webpack bundling
+      config.externals = config.externals || []
+      config.externals.push({
+        'pdf-parse': 'commonjs pdf-parse',
+        'mammoth': 'commonjs mammoth',
+      })
+    }
+    
+    // Ignore pdf-parse test directory
+    config.module = config.module || {}
+    config.module.rules = config.module.rules || []
+    config.module.rules.push({
+      test: /\.pdf$/,
+      type: 'asset/resource',
+    })
+    
+    return config
+  },
 }
 
 export default nextConfig
